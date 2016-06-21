2a08cee22c0b6fe91af02adec766a1e30f3d77f0
Dmitriy Melnichenko <tairezzzz@gmail.com>
2016-06-21 16:49:28 +0300

    Banners

--- src/app/components/banners/banners.service.js
+++ src/app/components/banners/banners.service.js
@@ -1,24 +1,45 @@
 export class BannersService {
-  constructor ($resource, $http) {
+  constructor ($http) {
     'ngInject';
 
     this.$http = $http;
     this.banners = {};
     this.onInit();
+    this.message = '';
   }
 
   onInit(){
-    this.getBanners();
+
+    this.$http.get('banners.json')
+      .then((response)=>{
+
+        angular.copy(response.data, this.banners)
+
+        //console.log(this.banners);
+      });
+
+
+
+    // this.$http.get('banners.json')
+    //   .success((response) => {
+    //
+    //     // this.banners = response;
+    //
+    //     angular.copy(response, this.banners);
+    //     //return response;
+    //   });
   }
 
-  getBanners() {
-    return this.$http.get('banners.json')
-      .success((response) => {
+  addBanner(data){
+    let company = data.companyName;
+    if (typeof this.banners[company] == 'object')
+      this.banners[company].push(data);
+    else
+      this.banners[company] = data;
 
-        // this.banners = response;
-
-        angular.copy(response, this.banners);
-        return response;
-    });
+    this.$http.post('banners.json', this.banners)
+      .then((response) => {
+        this.message = 'Баннер успешно добавлен.'
+      });
   }
 }
